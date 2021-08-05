963eb1824c92519e4c7fae2c5a24c6b16f394764
kudrinyaroslav <yykudrin@gmail.com>
2021-08-05 13:42:20 +0000

    added workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Hello $1" # uses the who-to-greet
 time=$(date) # gets the current time and sets it as an output
 echo "::set-output name=time::$time" #sets the action's output parameter
